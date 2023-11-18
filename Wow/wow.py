aa9c7e2f5ee249bca1d53725e0956fa94b382b97
Aliakbar Soltani <aliakbarsoltaniiiii@gmail.com>
2023-11-18 19:44:19 +0330

    add Wow

---
+++ Wow/wow.py
@@ -0,0 +1,10 @@
+x=int(input("Enter The Num : "))
+if x<=10 and x>0 :
+    i=0
+    print("W",end='')
+    while i<x :
+        print("o",end='')
+        i+=1
+    print("w!")
+else :
+    print("InCorrect")
